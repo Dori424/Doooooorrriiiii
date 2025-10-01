ccd1cacf0c01e1cf127a63c1af15f15df091acd5
Dori424 <haidarlatif59aa@gmail.com>
2025-10-01 12:34:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,7 @@
-// Edit this file to add/remove passwords.
-// Put as many plaintext passwords as you want in the array below.
-// NOTE: this file is served to clients, so anyone who can view it can see these values.
-// For better security, use server-side auth.
-
 const config = {
-  // Add new passwords here, e.g. "pass1", "anotherPass"
   passwords: [
     "mySecret123",
     "guestPass",
-    "Skolbög"
+    "anotherOne"
   ]
 };
